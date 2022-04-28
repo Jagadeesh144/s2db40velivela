a668c7112f2c2757434ad828d07911568c98a3a3
Jagadeesh144 <email@youremail.com>
2022-04-27 19:17:26 -0500

    added

--- routes/Ushodaya.js
+++ routes/Ushodaya.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const Ushodaya_controlers= require('../controllers/Ushodaya')
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
 
 
 router.get('/', Ushodaya_controlers.Ushodaya_view_all_Page);
